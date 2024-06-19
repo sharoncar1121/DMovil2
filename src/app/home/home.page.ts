07c87db963fb2ec50b4208a84b3c17c0d7f8e3f3
sharoncar1121 <109883948+sharoncar1121@users.noreply.github.com>
2024-06-18 22:30:32 -0600

    ejercicio5

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem } from '@ionic/angular/standalone';
+import{ Camera, CameraResultType} from '@capacitor/camera'
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent,IonItem],
 })
 export class HomePage {
+
+  imageSrc: string= '';
   constructor() {}
+  
+  async pickPhoto() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Uri,
+      saveToGallery: true,
+      promptLabelHeader: 'Seleccionar una opción',
+      promptLabelPicture: 'Tomar una foto',
+      promptLabelPhoto: 'Elegir de galería',
+    });
+
+    if (!image) return;
+
+    this.imageSrc = image.webPath ?? image.path ?? '';
+  }
+
 }
